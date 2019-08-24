eed4e15da60798728aac2a37243d6aefcdf0cb28
Wendler Eis <wendler_eis@hotmail.com>
2019-08-24 19:21:47 -0300

    feat: addo load action to repositories store

--- src/store/repositories.js
+++ src/store/repositories.js
@@ -1,12 +1,28 @@
-const LOAD_REPOSITORIES = 'github-client/repositories/LOAD_REPOSITORIES'
+import RepositoriesService from '../services/repositores'
+
+const LOAD = 'repositories/LOAD'
 
 export default function repositoriesReducer(state = {}, action = {}) {
   switch (action.type) {
+    case LOAD:
+      return { ...state, action }
     default:
       return state
   }
 }
 
-export function loadRepositories() {
-  return { type: LOAD_REPOSITORIES }
+export function loadRequest(user) {
+  const repositoriesService = new RepositoriesService()
+
+  return async dispatch => {
+    const response = await repositoriesService.get(user)
+    dispatch(loadSuccess(response))
+  }
+}
+
+function loadSuccess(repositores) {
+  return {
+    type: LOAD,
+    repositores
+  }
 }
