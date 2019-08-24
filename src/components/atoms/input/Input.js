9fc71a3b47dbfaa12aaad0ad2892b7ec87f387ee
Wendler Eis <wendler_eis@hotmail.com>
2019-08-23 21:54:13 -0300

    feat: add input component

---
+++ src/components/atoms/input/Input.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledInput = styled.input`
+  border: 0;
+  padding: 10px 15px;
+  background-color: #e3e3e3;
+  border-radius: 4px;
+  font-size: 16px;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 8px 0px rgba(255, 90, 73, 1);
+  }
+`
+
+export default function Input() {
+  return <StyledInput />
+}
