5b93745cb8edc9e615a52eaac6b1bc19ac3b2c9b
Wendler Eis <wendler_eis@hotmail.com>
2019-08-24 18:04:56 -0300

    feat: add button atom styles

--- src/components/atoms/button/Button.js
+++ src/components/atoms/button/Button.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const StyledButton = styled.button`
-  box-sizing: border-box;
-  background: ${props => (props.primary ? 'palevioletred' : '#eaeaea')};
-  color: ${props => (props.primary ? 'white' : 'palevioletred')};
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: ${props => (props.primary ? '2px solid palevioletred' : 'none')};
+  padding: 10px 15px;
+  border: 0;
   border-radius: 3px;
+  background-color: #f0f0f0;
+  color: #52bdb2;
+  text-transform: uppercase;
+  font-weight: 500;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background-color: #e0e0e0;
+    transition: all 0.5s;
+  }
+
+  ${props => {
+    return (
+      props.primary && css`
+        background: #ff5a49;
+        color: #ffffff;
+
+        &:hover {
+          background-color: #ff2c16;
+        }
+      `
+    )
+  }}
 `
 export default function Button(props) {
   return <StyledButton {...props} />
