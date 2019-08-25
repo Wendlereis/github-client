ef2e75a6e455610b805949cd56e8c2f881a68067
Wendler Eis <wendler_eis@hotmail.com>
2019-08-25 01:59:12 -0300

    feat: add card molecule

---
+++ src/components/molecules/card/Card.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { GoRepo, GoStar, GoRepoForked } from 'react-icons/go'
+
+import { Label } from '../../atoms/label'
+
+const StyledCard = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid #e0e0e0e0;
+  background-color: #ffffff;
+`
+
+const Row = styled.article`
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+
+  &:nth-child(1) {
+    label {
+      margin-left: 12px;
+    }
+  }
+
+  &:nth-child(3) {
+    label {
+      margin-left: 8px;
+      margin-right: 12px;
+    }
+  }
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`
+
+export default function Card({ repository }) {
+  return (
+    <StyledCard>
+      <Row>
+        <GoRepo size={24} />
+        <Label heading text={repository.name} />
+      </Row>
+      <Row>
+        <Label subheading text={repository.description} />
+      </Row>
+      <Row>
+        <GoStar size={18} />
+        <Label text={repository.stargazers_count} />
+
+        <GoRepoForked size={18} />
+        <Label text={repository.forks_count} />
+      </Row>
+    </StyledCard>
+  )
+}
+
+Card.propTypes = {
+  repository: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    stargazers_count: PropTypes.number.isRequired,
+    forks_count: PropTypes.number.isRequired
+  })
+}
