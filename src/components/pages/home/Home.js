199e4abe07c851929108995068fe3545ca67e2cf
Wendler Eis <wendler_eis@hotmail.com>
2019-08-25 02:38:21 -0300

    feat: add list of repositories in a card

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { HomeTemplate } from '../../templates/home'
+import { Card } from '../../molecules/card'
+
+const CardWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 24px;
+`
+
+const StyledLink = styled(Link)`
+  width: calc(50% - 24px);
+  margin: 12px;
+  text-decoration: none;
+`
 
 export default function Home() {
   const repositories = useSelector(
@@ -11,11 +26,13 @@ export default function Home() {
 
   return (
     <HomeTemplate>
-      {repositories.map(repo => (
-        <Link key={repo.id} to={`/repo/${repo.name}`}>
-          <li>{repo.name}</li>
-        </Link>
-      ))}
+      <CardWrapper>
+        {repositories.map(repo => (
+          <StyledLink key={repo.id} to={`/repo/${repo.name}`}>
+            <Card repository={repo} />
+          </StyledLink>
+        ))}
+      </CardWrapper>
     </HomeTemplate>
   )
 }
