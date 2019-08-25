bcb65a1ab99036bf3b0c58ad4146347fc242771e
Wendler Eis <wendler_eis@hotmail.com>
2019-08-25 01:02:11 -0300

    feat: add avatar atom

---
+++ src/components/atoms/avatar/Avatar.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Avatar from './Avatar'
+
+storiesOf('Avatar', module).add('default', () => (
+  <Avatar src="https://i.pravatar.cc/300" />
+))
