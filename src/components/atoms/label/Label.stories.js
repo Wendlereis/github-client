900dea044bbfa3b6c79cae183a184f8ded943ee3
Wendler Eis <wendler_eis@hotmail.com>
2019-08-25 01:02:39 -0300

    feat: add subtitle style to label atom

--- src/components/atoms/label/Label.stories.js
+++ src/components/atoms/label/Label.stories.js
@@ -2,4 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Label from './Label'
 
-storiesOf('Label', module).add('default', () => <Label text="label" />)
+storiesOf('Label', module)
+  .add('default', () => <Label text="label" />)
+  .add('title', () => <Label text="it's a title" title />)
+  .add('subtitle', () => <Label text="it's a subtitle" subtitle />)
