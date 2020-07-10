fdea2d60cb39c0bf3f68e91042361a986742720d
Wendler Eis <wendler_eis@hotmail.com>
2020-07-09 23:10:29 -0300

    feat: add project

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+import Repository from "../pages/Repository";
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Dashboard />
+      </Route>
+      
+      <Route path="/repository">
+        <Repository />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routes;
