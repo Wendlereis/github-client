84cf916827a15656a41a6ef04c3f62615cbe2236
Wendler Eis <wendler_eis@hotmail.com>
2020-07-22 19:04:56 -0300

    improvement(dashboard): add styles

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,59 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
+import logo from "../../assets/images/svg/logo.svg";
 
-import { Title } from './styles'
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => (
   <>
-    <Title>Explore repositorios no github</Title>
+    <img src={logo} alt="Github explorer" />
+    <Title>Explore repositórios no Github.</Title>
+
+    <Form>
+      <input type="text" placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="aa">
+        <img
+          src="https://avatars0.githubusercontent.com/u/6570553?s=460&u=363a4bb79dea480907f1fe6909a0e63247ed9b74&v=4"
+          alt="blabla"
+        />
+        <div>
+          <strong>Bla Bla</strong>
+          <p>Descricao top</p>
+        </div>
+
+        <FiChevronRight />
+      </a>
+
+      <a href="aa">
+        <img
+          src="https://avatars0.githubusercontent.com/u/6570553?s=460&u=363a4bb79dea480907f1fe6909a0e63247ed9b74&v=4"
+          alt="blabla"
+        />
+        <div>
+          <strong>Bla Bla</strong>
+          <p>Descricao top</p>
+        </div>
+
+        <FiChevronRight />
+      </a>
+
+      <a href="aa">
+        <img
+          src="https://avatars0.githubusercontent.com/u/6570553?s=460&u=363a4bb79dea480907f1fe6909a0e63247ed9b74&v=4"
+          alt="blabla"
+        />
+        <div>
+          <strong>Bla Bla</strong>
+          <p>Descricao top</p>
+        </div>
+
+        <FiChevronRight />
+      </a>
+    </Repositories>
   </>
 );
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,95 @@
 import styled from "styled-components";
+import { shade } from "polished";
 
 export const Title = styled.h1`
   font-size: 48px;
+  line-height: 56px;
+  max-width: 450px;
+  margin-top: 80px;
   color: #3a3a3a;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 715px;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: "#3a3a3a";
+
+    &::placeholder {
+      color: #d3d3d3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background-color: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #ffffff;
+    font-weight: bold;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: ${shade(0.2, "#04d361")};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  margin-bottom: 96px;
+  max-width: 715px;
+
+  a {
+    display: flex;
+    align-items: center;
+    background-color: #ffffff;
+    border-radius: 5px;
+    text-decoration: none;
+    padding: 25px;
+    width: 100%;
+
+    transition: transform 0.3s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong: {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
