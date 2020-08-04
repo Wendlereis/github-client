eaf40a5fa008fcff1eeb3bd869741cbf22c4b3c6
Wendler Eis <wendler_eis@hotmail.com>
2020-08-03 23:08:43 -0300

    feat: add localstorage

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useEffect } from "react";
 import { FiChevronRight } from "react-icons/fi";
 import logo from "../../assets/images/svg/logo.svg";
 
@@ -18,7 +18,20 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [repo, setRepo] = useState("");
   const [formError, setFormError] = useState("");
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem("@GithubExplorer:repositories");
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("@GithubExplorer:repositories", JSON.stringify(repositories));
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
